f630aa06c76851e3e2d155381c25f0b1b8a141f5
Shobuki <Shobuki@users.noreply.github.com>
2025-07-08 15:37:28 +0700

    cleaning

--- front-dealer/prisma/migrations/20241028092217_add_parent_category_id_to_product_category/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "ProductCategory" ADD COLUMN     "parentCategoryId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "ProductCategory" ADD CONSTRAINT "ProductCategory_parentCategoryId_fkey" FOREIGN KEY ("parentCategoryId") REFERENCES "ProductCategory"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- front-dealer/prisma/migrations/20241106045200_add_token_to_user_session/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "UserSession" ADD COLUMN     "token" TEXT;
